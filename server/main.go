3372d109de746f6cdcefc118b0251949cc2b3e5e
Thiago Araujo <thiagoaraujo43034@gmail.com>
2024-06-30 13:34:37 -0300

    <feat/build>:[server] Construido lado servidor com suas dependências

---
+++ server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net"
+	"context"
+	"gihub.com/wesleywillians/grpc-hello-go/pb"
+	"google.golang.org/grpc"
+)
+
+type server struct{
+	pb.UnimplementedHelloServiceServer
+}
+
+func (*server) Hello(ctx context.Context, request *pb.HelloResquest) (*pb.HelloResponse, error)  {
+		result := "hello "+request.GetName()
+
+		response := &pb.HelloResponse{
+			Msg: result,
+		}
+
+		return response, nil
+	}
+
+func main(){
+	listener, error := net.Listen("tcp", "0.0.0.0:50051")
+	if error != nil{
+		log.Fatalf("failed to List %v", error)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterHelloServiceServer(grpcServer, &server{})
+
+	if error := grpcServer.Serve(listener); error != nil {
+		log.Fatalf("failed to List %v", error)
+	}
+}
